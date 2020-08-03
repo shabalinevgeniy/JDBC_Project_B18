78ac51ae2ec1247f1d448f7dbad6eb85a94f6f72
evgeniyshabalin <shabalinevgeniy@gmail.comgit config --global user.name Evgeniy>
2020-08-03 12:06:41 -0400

    day1

--- src/test/java/com/cybertek/jdbc/Main.java
+++ src/test/java/com/cybertek/jdbc/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
         //Replace the IP with your own IP address
-        String connectionStr = "jdbc:oracle:thin:@52.71.242.164:1521:XE";
+        String connectionStr = "jdbc:oracle:thin:@YOUR_IP_HERE:1521:XE";
         String username = "hr";
         String password = "hr";
 
@@ -13,6 +13,9 @@ public class Main {
         Connection conn = DriverManager.getConnection(connectionStr,username,password);
         // CREATING SIMPLE STATEMENT (forward only)
         Statement stmt = conn.createStatement();
+        // CREATING STATEMENT with maximum flexibility of moving up and down from rows
+        //Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+
         // GETTING RESULTSET OF THE QUERY
         ResultSet rs = stmt.executeQuery("SELECT * FROM REGIONS");
         // initially the cursor is at the location before first line of the resulting table
@@ -22,9 +25,9 @@ public class Main {
         rs.next() ;
         // right now we are at first row. to get column data as String
         System.out.println("First column value " + rs.getString(1)
-                                        + " or " + rs.getString("REGION_ID") );
+                + " or " + rs.getString("REGION_ID") );
         System.out.println("SECOND column value " + rs.getString(2)
-                                        + " or " + rs.getString("REGION_NAME") );
+                + " or " + rs.getString("REGION_NAME") );
         rs.next() ;  // now moved to next row
         System.out.println("First column value " + rs.getString(1) + " or " + rs.getString("REGION_ID") );
         System.out.println("SECOND column value " + rs.getString(2) + " or " + rs.getString("REGION_NAME") );
